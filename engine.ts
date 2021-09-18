d1204f0a00f047864387d35b5cf8b9318faea7e3
rorticus <rorticus@gmail.com>
2021-09-18 16:07:30 -0400

    Initial files for MakeCode project

---
+++ engine.ts
@@ -0,0 +1,133 @@
+const mapWidth = 24;
+const mapHeight = 24;
+
+const map = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+];
+
+let posX = 22, posY = 12;  //x and y start position
+let dirX = -1, dirY = 0; //initial direction vector
+let planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+
+let time = 0; //time of current frame
+let oldTime = 0; //time of previous frame
+
+// Add your code here
+function draw(dst: Image) {
+    const w = dst.width;
+    const h = dst.height;
+
+    for(let x = 0; x < w; x++) {
+        //calculate ray position and direction
+        let cameraX = 2 * x / w - 1; //x-coordinate in camera space
+        let rayDirX = dirX + planeX * cameraX;
+        let rayDirY = dirY + planeY * cameraX;
+
+        //which box of the map we're in
+        let mapX = Math.floor(posX);
+        let mapY = Math.floor(posY);
+
+        //length of ray from current position to next x or y-side
+        let sideDistX;
+        let sideDistY;
+
+        //length of ray from one x or y-side to next x or y-side
+        let deltaDistX = Math.abs(1 / rayDirX);
+        let deltaDistY = Math.abs(1 / rayDirY);
+        let perpWallDist;
+
+        //what direction to step in x or y-direction (either +1 or -1)
+        let stepX;
+        let stepY;
+
+        let hit = 0; //was there a wall hit?
+        let side; //was a NS or a EW wall hit?
+
+        //calculate step and initial sideDist
+        if (rayDirX < 0) {
+            stepX = -1;
+            sideDistX = (posX - mapX) * deltaDistX;
+        }
+        else {
+            stepX = 1;
+            sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+        }
+        if (rayDirY < 0) {
+            stepY = -1;
+            sideDistY = (posY - mapY) * deltaDistY;
+        }
+        else {
+            stepY = 1;
+            sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+        }
+
+        //perform DDA
+        while (hit == 0) {
+            //jump to next map square, OR in x-direction, OR in y-direction
+            if (sideDistX < sideDistY) {
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
+            }
+            else {
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
+            }
+            //Check if ray has hit a wall
+            if (map[mapY * mapWidth + mapX] > 0) { hit = 1; }
+        }
+
+        //Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
+        if (side == 0) perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+        else perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+
+        //Calculate height of line to draw on screen
+        let lineHeight = Math.floor(h / perpWallDist);
+
+        //calculate lowest and highest pixel to fill in current stripe
+        let drawStart = -lineHeight / 2 + h / 2;
+        if (drawStart < 0) drawStart = 0;
+        let drawEnd = lineHeight / 2 + h / 2;
+        if (drawEnd >= h) drawEnd = h - 1;
+
+        //choose wall color
+        let color;
+        switch (map[mapY * mapWidth + mapX]) {
+            case 1: color = 2; break; //red
+            case 2: color = 3; break; //green
+            case 3: color = 4; break; //blue
+            case 4: color = 5; break; //white
+            default: color = 6; break; //yellow
+        }
+
+        //give x and y sides different brightness
+        if (side == 1) { color = color / 2; }
+
+        //draw the pixels of the stripe as a vertical line
+        dst.drawLine(x, drawStart, x, drawEnd, color);
+    }
+}
---
+++ main.ts
@@ -0,0 +1,62 @@
+const background = image.create(
+    screen.width,
+    screen.height
+);
+
+let lastFrame;
+let thisFrame = game.runtime();
+
+game.onUpdate(() => {
+    lastFrame = thisFrame;
+    thisFrame = game.runtime();
+    const frameTime = (thisFrame - lastFrame) / 1000.0; //frameTime is the time this frame has taken, in seconds
+
+    background.fillRect(0, 0, background.width, background.height, 0);
+    draw(background);
+
+    //speed modifiers
+    const moveSpeed = frameTime * 5.0; //the constant value is in squares/second
+    const rotSpeed = frameTime * 3.0; //the constant value is in radians/second
+
+    if (controller.up.isPressed()) {
+        if (map[Math.floor(posX + dirX * moveSpeed) + Math.floor(posY) * mapWidth] == 0) {
+            posX += dirX * moveSpeed;
+        }
+
+        if (map[Math.floor(posX) + Math.floor(posY + dirY * moveSpeed) * mapWidth] == 0) {
+            posY += dirY * moveSpeed;
+        }
+    }
+
+    //move backwards if no wall behind you
+    if (controller.down.isPressed()) {
+        if (map[Math.floor(posX - dirX * moveSpeed) + Math.floor(posY) * mapWidth] == 0) {
+            posX -= dirX * moveSpeed
+        }
+        if (map[Math.floor(posX) + Math.floor(posY - dirY * moveSpeed) * mapWidth] == 0) {
+            posY -= dirY * moveSpeed;
+        }
+    }
+    //rotate to the right
+    if (controller.right.isPressed()) {
+        //both camera direction and camera plane must be rotated
+        const oldDirX = dirX;
+        dirX = dirX * Math.cos(-rotSpeed) - dirY * Math.sin(-rotSpeed);
+        dirY = oldDirX * Math.sin(-rotSpeed) + dirY * Math.cos(-rotSpeed);
+        const oldPlaneX = planeX;
+        planeX = planeX * Math.cos(-rotSpeed) - planeY * Math.sin(-rotSpeed);
+        planeY = oldPlaneX * Math.sin(-rotSpeed) + planeY * Math.cos(-rotSpeed);
+    }
+    //rotate to the left
+    if (controller.left.isPressed()) {
+        //both camera direction and camera plane must be rotated
+        const oldDirX = dirX;
+        dirX = dirX * Math.cos(rotSpeed) - dirY * Math.sin(rotSpeed);
+        dirY = oldDirX * Math.sin(rotSpeed) + dirY * Math.cos(rotSpeed);
+        const oldPlaneX = planeX;
+        planeX = planeX * Math.cos(rotSpeed) - planeY * Math.sin(rotSpeed);
+        planeY = oldPlaneX * Math.sin(rotSpeed) + planeY * Math.cos(rotSpeed);
+    }
+});
+
+scene.setBackgroundImage(background);
